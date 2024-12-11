736b2ede56b6811f04224e95241f37a1341a2196
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2024-12-12 02:24:00 +0500

    Update Footer.tsx
    Footer Final

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -7,60 +7,70 @@ import tweet from "../../../Assets/tweet.png"
 
 export default function Footer(){
     return(
-        <div className="bg-black p-8 mt-10 ">
-        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 ">
-             <ul className="text-white mb-2 text-[14px] justify-center flex flex-col gap-6 xl:ml-4 ">
-                    <li>FIND A STORE</li>
-                    <li>BECOME A MEMBER</li>
-                    <li>SIGN UP FOR EMAIL</li>
-                    <li>SEND US FEEDBACK</li>
-                    <li>STUDENT DISCOUNT</li>
-                </ul>
-
-                <ul className="text-[#7E7E7E] text-[14px] justify-center flex flex-col gap-4 mt-6">
-                    <li className="text-white">GET HELP</li>
-                    <li>Order Status</li>
-                    <li>Delivery</li>
-                    <li>Returns</li>
-                    <li>Payment Options</li>
-                    <li>Contact Us On Nike.com Inquiries</li>
-                    <li>Contact Us On All Other Inquiries</li>
-                </ul>
-
-                <ul className="text-[#7E7E7E] text-[14px] justify-center flex flex-col gap-4 mb-[8%] mt-6 xl:mb-[23%] sm:mb-[32%]">
-                    <li className="text-white">ABOUT NIKE</li>
-                    <li>News</li>
-                    <li>Careers</li>
-                    <li>Investors</li>
-                    <li>Sustainability</li>
-                </ul>
-                
-
-                
-                <ul className="flex justify-center gap-4  sm:mt-[12%]">
-                    <li><Image src={tweet} alt="tweet" /></li>
-                    <li><Image src={fb} alt="fb" /></li>
-                    <li><Image src={yt} alt="yt" /></li>
-                    <li><Image src={insta} alt="insta" /></li>
-                </ul>
-        </div>
-
-<div className="sm:flex sm:items-center justify-between ">
-<div className="flex flex-col sm:flex-row sm:items-center sm:gap-2">
-        <div className="flex justify-center items-center mt-4">
+        <footer className="bg-black font-['Helvetica Neue'] text-white px-6 py-8 md:px-10 lg:px-16">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div>
+              {/* column 1 */}
+              <h3 className="mb-4 text-white text-lg font-semibold">FIND A STORE</h3>
+              <ul>
+                <li><a href="#" className="hover:underline leading-6">BECOME A MEMBER</a></li>
+                <li><a href="#" className="hover:underline leading-6">SIGN UP FOR EMAIL</a></li>
+                <li><a href="#" className="hover:underline leading-6">Send Us Feedback</a></li>
+                <li><a href="#" className="hover:underline leading-6">STUDENT DISCOUNTS</a></li>
+              </ul>
+            </div>
+            <div>
+  
+              {/* Column 2 */}
+              <h3 className="mb-4 text-white text-lg font-semibold">GET HELP</h3>
+              <ul className="text-gray-400">
+                <li><a href="#" className="hover:underline leading-6">Order Status</a></li>
+                <li><a href="#" className="hover:underline leading-6">Delivery</a></li>
+                <li><a href="#" className="hover:underline leading-6">Returns</a></li>
+                <li><a href="#" className="hover:underline leading-6">Payment Options</a></li>
+                <li><a href="/Contact" className="hover:underline leading-6">Contact Us On Nike.com Inquiries</a></li>
+                <li><a href="/Contact" className="hover:underline leading-6">Contact Us On All Other Inquiries</a></li>
+              </ul>
+            </div>
+            <div>
+  
+              {/* Column 3 */}
+              <h3 className="mb-4 text-white text-lg font-semibold">ABOUT NIKE</h3>
+              <ul className="text-gray-400">
+                <li><a href="#" className="hover:underline leading-6">News</a></li>
+                <li><a href="#" className="hover:underline leading-6">Careers</a></li>
+                <li><a href="#" className="hover:underline leading-6">Investors</a></li>
+                <li><a href="#" className="hover:underline leading-6">Sustainability</a></li>
+              </ul>
+            </div>
+             
+             {/* social link */}
+            <div className="flex flex-col justify-between items-start lg:items-end">
+              <div className="flex space-x-4">
+                <a href="#" className="hover:underline"><Image src={tweet} alt="Twitter" height={24} width={24} /></a>
+                <a href="#" className="hover:underline"><Image src={fb} alt="Facebook" height={24} width={24} /></a>
+                <a href="#" className="hover:underline"><Image src={yt} alt="YouTube" height={24} width={24} /></a>
+                <a href="#" className="hover:underline"><Image src={insta} alt="Instagram" height={24} width={24} /></a>
+              </div>
+            </div>
+          </div>
+  
+          {/* Bottom */}
+          <div className="mt-8 flex flex-col md:flex-row justify-between items-center text-sm text-gray-400">
+          <div className="flex justify-center items-center mt-4">
             <Image src={location} alt="pak" className="w-6 h-5"/>
             <p className="text-white text-[14px]">Pakistan</p>
         </div>
-        <p className="text-[#7E7E7E] text-[12px] mt-2 sm:mt-4">&copy; 2024 Huzaifa Ghouri. All Rights Reserved</p>
+            <div className="mb-4 md:mb-0">&copy; 2024 Huzaifa Ghouri. All Rights Reserved</div>
+            <div className="flex flex-wrap space-x-4 justify-center md:justify-end">
+              <a href="#" className="hover:underline">Guides</a>
+              <a href="#" className="hover:underline">Terms of Sale</a>
+              <a href="#" className="hover:underline">Terms of Use</a>
+              <a href="#" className="hover:underline">Nike Privacy Policy</a>
+            </div>
+          </div>
         </div>
-        <ul className="text-[#7E7E7E] text-[11px] flex mt-2 gap-4 sm:text-[12px] sm:mt-4">
-            <li>Guides</li>
-            <li>Terms of Sale</li>
-            <li>Terms of Use</li>
-            <li>Nike Privacy Policy</li>
-        </ul>
-        </div>
-        </div>
-        
-    )
-}
+      </footer>
+    );
+  };
